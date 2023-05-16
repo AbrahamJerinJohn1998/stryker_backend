a8787a912216dda41e636028504df5e5fac2b2df
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2023-05-16 15:32:47 +0530

    initial

---
+++ src/main/java/com/example/strykerapp_backend/StrykerappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.strykerapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrykerappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrykerappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/strykerapp_backend/StrykerappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strykerapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrykerappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
